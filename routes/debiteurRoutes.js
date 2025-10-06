36e69bf500ef695583d9272e5a16b3cba0359675
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2025-10-06 10:10:18 +0000

    suppression des dossiers unitiles et creations du controller de recherche de dossiers debieur

--- routes/debiteurRoutes.js
+++ routes/debiteurRoutes.js
@@ -6,6 +6,11 @@ const debiteurController = require('../controllers/debiteur');  // Importer le c
 // Route pour récupérer les débiteurs
 router.get('/debiteurs/:sitename', debiteurController.getDebiteurs);
 // Route pour récupérer un débiteur par ID
-router.get('/debiteurs/:sitename/:debiteurId', debiteurController.getDebiteurs);
+// router.get('/debiteurs/:sitename/:debiteurId', debiteurController.getDebiteurs);
+
+//-------Rechercher les dossier debiteur avec différents paramètre -----------//
+router.get('/debiteurs/:sitename/:dossiers', debiteurController.getDossiers);
+
+
 
 module.exports = router;
