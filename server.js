510a809a19cd8ea0bc4c431cfc26ef0a81f4392d
ckonate <constant.konate@sertia.fr>
2025-11-06 22:10:45 +0100

    update CORS for Angular frontend

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 //app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:4200', 'https://aden-portal-git-deploy-kconstants-projects.vercel.app'],
+  origin: ['http://localhost:4200', 'https://aden-portal.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
